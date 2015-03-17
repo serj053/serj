c8fc58d3af4e51dfda9385c7abe7731a64b5339e
serj053 <serj053@yandex.ru>
2015-03-17 20:01:28 +0400

    rename file

---
+++ view/new_art.php
@@ -0,0 +1,36 @@
+
+<html>
+<head><title> </title>
+    <link href="../css/main.css" rel="stylesheet" type="""text/css"/>
+</head>
+<body>
+<div id="wrapper">
+    <div id="cap">
+
+    </div>
+    <hr>
+    <div id="menu">
+        <a href="../index.php">Главная </a>|
+        <a href="../index.php?ctrl=News/getAll">Статьи </a>|
+        <a href="">Что новенького </a>|
+        <a href="">Зачем живем </a>|
+        <a href="">Прочее </a>
+    </div>
+    <hr>
+    <div id="edit">
+        <a href="view/new_art.php">Вставить статью</a>|
+        <a href="">Редактировать</a>|
+        <a href="">Удалить</a>
+    </div>
+    <div id="all_news">
+        <div>
+            <form action="" method="post" >
+                <input type="text" name="title" id=""/>
+                <textarea name="content"></textarea>
+            </form>
+        </div>
+    </div>
+</div><!--end wrapper -->
+</body>
+</html>
+
